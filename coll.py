3d21f673195d2088eab7fbf4717b5d21999eef16
nastyaPuf <47403051+nastyaPuf@users.noreply.github.com>
2019-12-03 11:17:51 +0300

    Add files via upload

---
+++ coll.py
@@ -0,0 +1,144 @@
+#Настя
+
+import re
+
+chp_sep_word = "Глава [\d\.'I''V''X']* "
+art_sep_word = "Статья [\d\.]* "
+par_sep_word = "\n\d\. "
+
+class Collection:  #класс для коллекции
+    def __init__(self, text):
+        self.text = text
+        self.d = {}
+
+
+    def itersplit(self, sep):  #функция для разделения текста по sep и возвращаю по одному
+        exp = re.compile(sep)
+        if exp.search(self.text, 0) is None:
+            return
+        pos = 0
+        m1 = exp.search(self.text, 0)
+        while True:
+            m = exp.search(self.text, pos)
+            if not m:
+                yield self.text[pos:], m1[0]
+                break
+            yield self.text[pos:m.start()], m1[0]
+            pos = m.end()
+            m1 = m
+
+
+def iter_by_chapter(collect, doc_id): #итерирование по главам с возвращением двух словрей -- d[текст] = номер главы,
+                                        # d[номер главы] = текст
+    d_chp, d_rev = {}, {}
+    for i, num_chp in collect.itersplit(chp_sep_word):
+        num_chp = num_chp.split(' ')[1]
+        d_chp[doc_id, num_chp] = i
+        d_rev[i] = (doc_id, num_chp)
+    return d_chp, d_rev
+
+
+def iter_by_art(collect, doc_id):  #возвращение словарей с номером главы и статьи
+    d_art, d_rev = {}, {}
+    for i, num_chp in collect.itersplit(chp_sep_word):
+        num_chp = num_chp.split(' ')[1]
+        new_col = Collection(i)
+        for j, num_art in new_col.itersplit(art_sep_word):
+            num_art = num_art.split(' ')[1]
+            d_art[(doc_id, num_chp, num_art)] = j
+            d_rev[j] = (doc_id, num_chp, num_art)
+    return d_art, d_rev
+
+def iter_by_par(collect, doc_id):  #возвращение словарей с номером главы, статьи и пункта
+    d_par, d_rev = {}, {}
+    for i, num_chp in collect.itersplit(chp_sep_word):
+        new_col = Collection(i)
+        num_chp = num_chp.split(' ')[1]
+        for j, num_art in new_col.itersplit(art_sep_word):
+            num_art = num_art.split(' ')[1]
+            new_col2 = Collection(j)
+            for f, num_par in new_col2.itersplit(par_sep_word):
+                num_par = re.search('[\d\.]', num_par)[0]
+                d_par[(doc_id, num_chp, num_art, num_par)] = f
+                d_rev[f] = (doc_id, num_chp, num_art, num_par)
+    return d_par, d_rev
+
+
+def chp(col): #итерирование по главам
+    for i, num_chp in col.itersplit(chp_sep_word):
+        yield i
+
+def art(col): #итерирование по статьям
+    for i, num_chp in col.itersplit(chp_sep_word):
+        new_col = Collection(i)
+        for j, num_art in new_col.itersplit(art_sep_word):
+            yield j
+
+def par(col): #итерирование по пунктам
+    for i, num_chp in col.itersplit(chp_sep_word):
+        new_col = Collection(i)
+        for j, num_art in new_col.itersplit(art_sep_word):
+            new_col2 = Collection(j)
+            for f, num_par in new_col2.itersplit(par_sep_word):
+                yield f
+
+
+def iter_by_docs(docs, dir, iter_by, it): #итерирование по документам, в параметрах по чему итерироваться(iter_by), и что именно нужно сделать при итерировании -- вернуть словарь(it=0) или постепенно каждую часть(it=1)
+    doc_id = docs[docs.find('_') + 1: docs.find('.')]
+    f = open(dir + '/' + docs, 'r')
+    file = f.read()
+    c = Collection(file)
+    if it == 0:
+        if iter_by == 'chapter':
+            return iter_by_chapter(c, doc_id)
+        elif iter_by == 'paragraph':
+            return iter_by_par(c, doc_id)
+        elif iter_by == 'article':
+            return iter_by_art(c, doc_id)
+        else:
+            print("MISTAKE")
+            return
+    else:
+        if iter_by == 'chapter':
+            return chp(c)
+        elif iter_by == 'paragraph':
+            return par(c)
+        elif iter_by == 'article':
+            return art(c)
+        else:
+            print("MISTAKE")
+            return
+
+
+
+'''
+dir = "codexes"
+for file in os.listdir(dir):
+    for i_d in iter_by_docs(file, dir, 'chapter', 1):
+        print(i_d)
+        print('_______')
+    break
+'''
+
+
+
+'''
+file = open("codexes/codex_1.txt", 'r')
+iter_docs("codex_1.txt", dir)
+f = file.read()
+coll = Collection(f)
+chp_sep_word = "Глава [\d\.]* "
+art_sep_word = "Статья [\d\.]* "
+par_sep_word = "\n\d\. "
+
+J, Z = iter_by_chapter(coll, 1)
+print(J.keys())
+'''
+
+'''
+for k in par(coll):
+    print(k)
+    print('---')
+'''
+
+
---
+++ tokenize_docs.py
@@ -0,0 +1,41 @@
+#Настя
+
+import re
+import nltk
+import os
+
+from nltk.corpus import stopwords
+from pymorphy2 import MorphAnalyzer
+from string import punctuation
+from coll import iter_by_docs
+
+class Tokenizer:
+    def __init__(self, text):
+        self.text = text
+
+    def tokenize(self, cash, morph, stop_words):
+        tokens = nltk.word_tokenize(self.text)
+        tokens = [re.sub(r'\s+', ' ', i) for i in tokens]
+        tokens = [re.sub(r'[^\w\s]', ' ', i) for i in tokens]
+        tokens = [token for token in tokens if token not in stop_words\
+                  and token != " " and token.strip() not in punctuation]
+        for i in range(len(tokens)):
+            if tokens[i] in cash:
+                tokens[i] = cash[tokens[i]]
+            else:
+                cash[tokens[i]] = morph.parse(tokens[i])[0].normal_form
+                tokens[i] = cash[tokens[i]]
+        return tokens
+
+def docs_parser(dir):
+    cash = dict()
+    morph = MorphAnalyzer()
+    stop_words = stopwords.words("russian")
+    for file in os.listdir(dir):
+        for i_d in iter_by_docs(file, dir, 'chapter', 1):
+            t = Tokenizer(i_d)
+            print(t.tokenize(cash, morph, stop_words))
+
+
+dir = "codexes"
+docs_parser(dir)
