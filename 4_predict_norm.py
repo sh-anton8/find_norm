e5889d7bbbcde2d54d54f384a6eb52b6d6c1ebeb
nastyaPuf <nastya-bori@yandex.ru>
2020-04-13 15:47:26 +0300

    Add task

--- 4_predict_norm.py
+++ 4_predict_norm.py
@@ -1,6 +1,12 @@
 import tools.tokenize_docs as tokenize_docs
 from tools.search import Inv_Ind_Search
 from tools.search import TFIDF_Search
+import os
+
+from tools.relative_paths_to_directories import path_to_directories, CNT_ARTICLES
+
+PATH_TO_ROOT, PATH_TO_TOOLS, PATH_TO_FILES, PATH_TO_TF_IDF, PATH_TO_INV_IND, PATH_TO_BM_25,\
+    PATH_TO_LEARNING_TO_RANK = path_to_directories(os.getcwd())
 
 
 # директория на папку с кодексами
@@ -8,14 +14,14 @@ fnCollectionDir = "codexes"
 
 # файл, куда будет сохраняться (или где уже сохранен) объект класса InvIndex
 # перед поиском рекомендуется удалять имеющийся файл и считать заново (иначе могут быть проблемы в директориях)
-fnIdxCodex2Article = "idx/codex2article.pickle"
+fnIdxCodex2Article = PATH_TO_INV_IND
 
 # файл, куда будет сохраняться (или где уже сохранен) объект класса InvIndex
 # перед поиском рекомендуется удалять имеющийся файл и считать заново (иначе могут быть проблемы в директориях)
-fnTFIDFCodex2Article = "tfidf/tdidf_codex2article.pickle"
+fnTFIDFCodex2Article = os.path.join(PATH_TO_TF_IDF, "tf_idf")
 
 # директория на pravoved
-pravovedDir = "files/pravoved_articles.txt"
+pravovedDir = os.path.join(PATH_TO_FILES, 'pravoved_articles.txt')
 
 
 # Пример поиска основанного на обратном индексе
