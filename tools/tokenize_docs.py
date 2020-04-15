9cade5b1921797e99170e6b14e8a76c6952ac934
Anton Shapkin <57372391+sh-anton8@users.noreply.github.com>
2020-04-15 21:29:50 +0500

    Update tokenize_docs.py

--- tools/tokenize_docs.py
+++ tools/tokenize_docs.py
@@ -7,35 +7,26 @@ import os
 from nltk.corpus import stopwords
 from pymorphy2 import MorphAnalyzer
 from string import punctuation
-from tools.coll import iter_by_docs
+
 
 class Tokenizer:
-    def __init__(self, text):
-        self.text = text
-
-    def tokenize(self, cash, morph, stop_words):
-        tokens = nltk.word_tokenize(self.text)
-        tokens = [re.sub(r'\s+', ' ', i) for i in tokens]
-        tokens = [re.sub(r'[^\w\s]', ' ', i) for i in tokens]
-        tokens = [token for token in tokens if token not in stop_words\
-                  and token != " " and token.strip() not in punctuation]
-        for i in range(len(tokens)):
-            if tokens[i] in cash:
-                tokens[i] = cash[tokens[i]]
+    def __init__(self, morph=None, stop_words=None):
+        self.morph = morph if morph else MorphAnalyzer()
+        self.stop_words = stop_words if stop_words else stopwords.words("russian")
+        self.cache = {}
+        
+    def tokenize(self, text):
+        tokens = nltk.word_tokenize(text)
+        tokens = [re.sub(r'\s+', ' ', t) for t in tokens]
+        tokens = [re.sub(r'[^\w\s]', ' ', t) for t in tokens]
+        tokens = [t for t in tokens if t not in self.stop_words \
+                  and t and t.strip() not in punctuation]
+        result = []
+        for t in tokens:
+            if t in self.cache:
+                result.append(self.cache[t])
             else:
-                cash[tokens[i]] = morph.parse(tokens[i])[0].normal_form
-                tokens[i] = cash[tokens[i]]
-        return tokens
-
-def docs_parser(dir):
-    cash = dict()
-    morph = MorphAnalyzer()
-    stop_words = stopwords.words("russian")
-    for file in os.listdir(dir):
-        for i_d in iter_by_docs(file, dir, 'chapter', 1):
-            t = Tokenizer(i_d)
-
-'''
-dir = "codexes"
-docs_parser(dir)
-'''
+                nf = self.morph.parse(t)[0].normal_form
+                self.cache[t] = nf
+                result.append(nf)
+        return result
