a2d56d7920dcc0db0ca185a589b0f98e0dc5907d
Anton Shapkin <57372391+sh-anton8@users.noreply.github.com>
2020-04-15 21:29:22 +0500

    Update tfidf.py

--- tools/tfidf.py
+++ tools/tfidf.py
@@ -3,43 +3,60 @@
 import pickle
 import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
-from tools.inverse_index import InvIndex
+from sklearn.metrics.pairwise import cosine_similarity
 from tqdm import tqdm
 
 
 class TFIDF:
-    def __init__(self, inverse_index_pickle, ngramm=(1, 1), norm='l2', use_idf=True,
-                 sublinear_tf=False):
-        self.vectorizer = TfidfVectorizer(preprocessor=None, encoding='utf-8', ngram_range=ngramm,
-                                          norm=norm, use_idf=use_idf, sublinear_tf=sublinear_tf)
-        self.tfidf_matrix = np.array([])
-        self.num_to_num_dict = {}
-        self.inverse_index = InvIndex.load(inverse_index_pickle)
+    def __init__(self, tokenizer, vectorizer_params=None):
+        self.tokenizer = tokenizer
+        self.doc_ids = []
+        if vectorizer_params:
+            self.vectorizer = TfidfVectorizer(**vectorizer_params)
+        else:
+            self.vectorizer = TfidfVectorizer()
+        
+    def build_on(self, corpus, tokenized=True):
+        """
+        Построить обратный индекс по корпусу.
+        tokenized: если True, то считаем что подается уже токенизированный
+        корпус (тем же токенайзером что задан в init)
+        """
 
-    def count_tf_idf(self):
-        corpus = []
-        ind = 0
-        ta = tqdm(total=len(list(self.inverse_index.num_tokens_dict.keys())))
-        for key in list(self.inverse_index.num_tokens_dict.keys()):
-            corpus.append(" ".join(self.inverse_index.num_tokens_dict[key]))
-            self.num_to_num_dict[ind] = key
-            ind += 1
-            ta.update(1)
-        self.tfidf_matrix = self.vectorizer.fit_transform(corpus)
-        ta.close()
+        # для sklearn.TfidfVectorizer приходится подавать токенизированный
+        # корпус как склеенные строки токенов.
+        # в gensim проще, можно сразу подавать списки токенов
+        joined_corpus = []
+        for doc_id, doc_text in corpus:
+            self.doc_ids.append(doc_id)
 
-    def request_counting(self, request):
-        request_tfidf = self.vectorizer.transform(request)
-        return request_tfidf
+            tokens = doc_text if tokenized else self.tokenizer.tokenize(doc_text)
+            joined_corpus.append(" ".join(tokens))
+        self.tfidf_matrix = self.vectorizer.fit_transform(joined_corpus)
 
-    # сохраняем весь объект
+        
+    def search(self, query, topN=10, threshold=0.5):
+        """
+        Поиск по запросу.
+        topN - сколько документов оставить после ранжирования по релевантности
+        threshold - порог отсечения по релевантности.
+        Релевантность здесь - косинусная мера между вектором запроса и документа.
+        """
+        tokens = self.tokenizer.tokenize(query)
+       
+        query_tfidf = self.vectorizer.transform([" ".join(tokens)])
+        raw_sims = cosine_similarity(query_tfidf, self.tfidf_matrix).reshape(-1)
+        sims_indices = (-raw_sims).argsort()[:topN]
+        # вернуть результат как список [(doc_id, rel), ...]
+        sims = [(self.doc_ids[si], raw_sims[si]) for si in sims_indices \
+               if raw_sims[si] >= threshold]
+        return sims
+
     def save(self, file):
         print('Saving tf-idf to: {}'.format(file))
         with open(file, 'wb') as f:
             pickle.dump(self, f)
 
-    # загружаем весь объект, использовать можно потом только его часть
-    # для этого используем модификатор @staticmethod
     @staticmethod
     def load(file):
         print('Loading tfidf from: {}'.format(file))
