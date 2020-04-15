d84dcc4637e4f3f620585d66658a2a9fce62408e
Anton Shapkin <57372391+sh-anton8@users.noreply.github.com>
2020-04-15 21:32:03 +0500

    Update 3_build_tfidf.py

--- 3_build_tfidf.py
+++ 3_build_tfidf.py
@@ -1,24 +1,28 @@
-import os
+from tools.simple_corp import SimpleCorp
+from tools.tokenize_docs import Tokenizer
 from tools.tfidf import TFIDF
+import os
 
 
 from tools.relative_paths_to_directories import path_to_directories
 PATH_TO_ROOT, PATH_TO_TOOLS, PATH_TO_FILES, PATH_TO_TF_IDF, PATH_TO_INV_IND, PATH_TO_BM_25, \
     PATH_TO_LEARNING_TO_RANK = path_to_directories(os.getcwd())
+
 # Пример того, как можно посчитать и сохранить в файл TFIDF-класс
 
-# директория файла с Обратным инждексом (объектом класса InvIndex)
-fnIdxCodex2Article = PATH_TO_INV_IND
+# директория на папку с кодексами
+codexes_dir = os.path.join(PATH_TO_ROOT, "codexes")
 
-# файл, куда будет сохраняться (или где уже сохранен) объект класса InvIndex
-# перед поиском рекомендуется удалять имеющийся файл и считать заново (иначе могут быть проблемы в директориях)
-fnTFIDFCodex2Article = os.path.join(PATH_TO_TF_IDF, 'tf_idf')
+simple_corp = SimpleCorp.load('codexes_corp_articles', f'{PATH_TO_FILES}\corp')
+tokenized_corp = SimpleCorp.load('codexes_tokenized_corp_articles', f'{PATH_TO_FILES}\corp')
 
-# если объект уже создан, то строить нет смысла
-# иначе строим и сохраняем объект класса InvIndex по директории fnIdxCodex2Article
-if not os.path.isfile(fnTFIDFCodex2Article):
-    # первый параметр - директория файла с Обратным инждексом
-    # второй параметр - ngramm
-    tf = TFIDF(fnIdxCodex2Article, (1, 1))
-    tf.count_tf_idf()
-    tf.save(fnTFIDFCodex2Article)
+tokenizer = Tokenizer()
+
+tfidf = TFIDF(tokenizer=tokenizer)
+tfidf.build_on(tokenized_corp, tokenized=True)
+tfidf.save(f'{PATH_TO_TF_IDF}\\tf_idf_1')
+
+query = 'Симметричные корректировки осуществляются в порядке, установленном настоящей статьей.'
+search_result = tfidf.search(query, threshold=0.1)
+found_articles = [(simple_corp.get_doc(doc_id), rel) for doc_id, rel in search_result]
+print(search_result)
