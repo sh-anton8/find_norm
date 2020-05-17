ec6a20d04781ddd4390940481117fcc322b0125d
Vlad Yurchenko <35797275+v-yurchenko@users.noreply.github.com>
2020-05-17 14:28:45 +0300

    Add files via upload

--- pi_9_predict_norm_LTR.py
+++ pi_9_predict_norm_LTR.py
@@ -8,11 +8,16 @@ import pandas as pd
 from tools.simple_corp import SimpleCorp
 from tools.name_codexes import name_codexes
 
+from flask import Markup
+
+
+from tools.relative_paths_to_directories import path_to_directories
+PATH_TO_TFIDF = "files/tf_idf/tf_idf_{}"
 PATH_TO_ROOT, PATH_TO_TOOLS, PATH_TO_FILES, PATH_TO_TF_IDF, PATH_TO_INV_IND, PATH_TO_BM_25, \
     PATH_TO_LEARNING_TO_RANK = path_to_directories(os.getcwd())
 
 
-def predict_norm(request, features=None):
+def predict_norm(request, features=None, iifh=None):
     new_request = Request(request, "", "").create_dict()
 
     xgb_model = joblib.load("final_xgb_model.sav")
@@ -29,7 +34,7 @@ def predict_norm(request, features=None):
 
     corpus = SimpleCorp.load("codexes_corp_articles", os.path.join(PATH_TO_FILES, "corp"))
     art_names = SimpleCorp.load('codexes_corp_art_names', f'{PATH_TO_FILES}/corp')
-
+    
     prediction_answer = []
     for p, doc_id in zip(pred, list(corpus.corpus.keys())):
         prediction_answer.append((doc_id, p))
@@ -40,7 +45,13 @@ def predict_norm(request, features=None):
     for i, res in enumerate(prediction_answer[:5]):
         doc_id = res[0]
         cod = name_codexes[int(doc_id[0])]
-        answer = f"{i + 1}. Cтатья {doc_id[1]}, {art_names.get_doc(doc_id)} // {cod[0]}{cod[1:].lower()}."
+        answer = f"<p>Cтатья {doc_id[1]}. {art_names.get_doc(doc_id)} // {cod[0]}{cod[1:].lower()}.</p>"
         print(answer)
-        valid_answers.append(answer)
+        if iifh is not None:
+            snippet = iifh.hightlight_words(request, doc_id)
+            answer += f"<p style='padding-left:50px; border-left: 3px gray;'>{snippet}</p>"
+            print(snippet)
+        valid_answers.append(Markup(answer))
     return valid_answers
+
+#predict_norm('время подачи заявления', features)
--- web_service.py
+++ web_service.py
@@ -1,19 +1,28 @@
 from flask import Flask, request, render_template, g
 from pi_9_predict_norm_LTR import predict_norm
 from tools.tfidf import TFIDF
-from tools.inverse_index import InvIndex
+from tools.inverse_index import InvIndex, InvertIndexForHighlight
 
+import os
+from tools.relative_paths_to_directories import path_to_directories
 PATH_TO_TFIDF = "files/tf_idf/tf_idf_{}"
+PATH_TO_ROOT, PATH_TO_TOOLS, PATH_TO_FILES, PATH_TO_TF_IDF, PATH_TO_INV_IND, PATH_TO_BM_25, \
+    PATH_TO_LEARNING_TO_RANK = path_to_directories(os.getcwd())
+
+from flask import Markup
+
+Markup('<p> flkfjlj </p>').unescape()
 
 app = Flask(__name__)
 features = []
-
+iifh = InvertIndexForHighlight.load(os.path.join(PATH_TO_FILES, "IIHighlight"))
 
 @app.before_first_request
 def load_tfidf():
     for i in range(1, 7):
         features.append(TFIDF.load(PATH_TO_TFIDF.format(i)))
     features.append(InvIndex.load("files/inv_ind.pickle"))
+
 
 
 @app.route('/', methods=['GET'])
@@ -24,7 +33,7 @@ def upload_file():
 @app.route('/', methods=['POST'])
 def main_page():
     text = request.form['text']
-    answers = predict_norm(text, features)
+    answers = predict_norm(text, features, iifh)
     print(answers)
     return render_template('upload.html', answers=answers, text=text)
 
